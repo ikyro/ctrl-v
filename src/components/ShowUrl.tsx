d4da02836712fe292dbfde15427343724686414c
ikyro <ikyroch@gmail.com>
2022-12-07 17:04:37 -0300

    fixed: base64 doesnt copy to clipboard

--- src/components/ShowUrl.tsx
+++ src/components/ShowUrl.tsx
@@ -1,4 +1,3 @@
-import { MouseEvent } from 'react'
 import toast from 'react-hot-toast'
 
 export const ShowUrl = ({
@@ -8,14 +7,11 @@ export const ShowUrl = ({
   url?: string
   placeholder: string
 }) => {
-  const handleClick = async (event: MouseEvent<HTMLElement>) => {
-    const [input] = event.currentTarget.children
-    const { value } = input as HTMLInputElement
-
-    if (!value.length) return
+  const handleClick = async () => {
+    if (!url?.length) return
 
     await navigator.clipboard
-      .writeText(value)
+      .writeText(url)
       .then(() => toast.success('Copied to clipboard!'))
       .catch(() => toast.error('Failed to copy in to clipboard!'))
   }
@@ -25,7 +21,7 @@ export const ShowUrl = ({
       <input
         type='text'
         placeholder={placeholder}
-        className='peer w-72 border-b-2 border-gray-300 p-0.5 font-grotesk text-sm leading-tight tracking-tight shadow-md outline-none hover:cursor-pointer md:w-80 lg:w-96 md:text-sm'
+        className='peer w-72 truncate border-b-2 border-gray-300 p-0.5 font-grotesk text-sm leading-tight tracking-tight shadow-md outline-none hover:cursor-pointer md:w-80 md:text-sm lg:w-96'
         defaultValue={url}
         disabled
       />
