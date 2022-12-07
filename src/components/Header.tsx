28469085a9c414c84dd1a33947e280f874510f8d
ikyro <ikyroch@gmail.com>
2022-12-07 16:32:58 -0300

    feat(components): better mobile version

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className='absolute top-20 md:top-14 lg:top-28'>
+      <h1 className='font-grotesk text-3xl font-bold leading-tight tracking-tight'>
+        CTRL + V
+      </h1>
+    </header>
+  )
+}
