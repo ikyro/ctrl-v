f192f9485fbf20f51758a21372cb7b42f7edb475
ikyro <ikyroch@gmail.com>
2022-12-07 16:33:13 -0300

    feat(components): better mobile version

--- src/components/SelectFiles.tsx
+++ src/components/SelectFiles.tsx
@@ -6,14 +6,17 @@ export const SelectFiles = ({
   onClick: HTMLAttributes<HTMLButtonElement>['onClick']
 }) => {
   return (
-    <button className='absolute top-0.5 -right-7' onClick={onClick}>
+    <button
+      className='absolute top-[1px] right-7 md:top-1.5 md:right-12 lg:right-20'
+      onClick={onClick}
+    >
       <svg
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
         viewBox='0 0 24 24'
         strokeWidth={1.5}
         stroke='currentColor'
-        className='h-6 w-6'
+        className='h-7 w-7'
       >
         <path
           strokeLinecap='round'
