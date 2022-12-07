061bab49d0225e0d72ee478e47da33d84e9ee24c
ikyro <ikyroch@gmail.com>
2022-12-07 07:45:23 -0300

    feat: upload components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
+import { Toaster } from 'react-hot-toast'
+import { Form } from './components/Form'
+import { Preview } from './components/Preview'
+import { SelectFiles } from './components/SelectFiles'
+import { useClipboard } from './hooks/useClipboard'
+
 const App = () => {
+  const { clipboard, handleCopyFromLocalFiles, handleDragOver, handleDrop } =
+    useClipboard()
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <div
+        className='grid min-h-screen place-items-center'
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <h1 className='absolute top-[4.5rem] font-grotesk text-3xl font-bold leading-tight tracking-tight lg:top-40'>
+          CTRL + V
+        </h1>
+        <main className='relative'>
+          <Form />
+
+          <SelectFiles onClick={handleCopyFromLocalFiles} />
+
+          <Preview clipboard={clipboard} />
+        </main>
+      </div>
+      <Toaster
+        position='top-right'
+        toastOptions={{
+          icon: '📋',
+          style: {
+            background: '#000',
+            color: '#fff',
+          },
+        }}
+      />
+    </>
   )
 }
 
