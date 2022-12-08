cf2dddcfc6cb75d2b09bb27dcb94bf9d7ad489b7
ikyro <ikyroch@gmail.com>
2022-12-08 17:02:15 -0300

    feat(test): working in tests

---
+++ src/test/App.test.tsx
@@ -0,0 +1,6 @@
+import { cleanup } from '@testing-library/react'
+import { afterEach, test } from 'vitest'
+
+afterEach(() => cleanup())
+
+test('')
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePluginFonts } from 'vite-plugin-fonts'
@@ -12,4 +14,7 @@ export default defineConfig({
       },
     }),
   ],
+  test: {
+    environment: 'happy-dom'
+  }
 })
